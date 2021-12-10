f4fd623dbf226f5d182766556811d63fbae1dd63
Dayton-Killian-1 <dayton.killian@chicagobooth.edu>
2021-12-10 08:55:28 +0000

    users pages are being built out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,36 @@
 class UsersController < ApplicationController
 
+  def new_registration_form
+    render({ :template => "user_templates/signup_form.html.erb"})
+  end
 
+  def create
+    user = User.new
+
+    user.name = params.fetch("input_username")
+    user.password = params.fetch("input_password")
+    user.password_confirmation = params.fetch("input_password_confirmation")
+    user.phone = params.fetch("input_phonenum")
+
+    save_status = user.save
+
+    if save_status == true
+      redirect_to("/users/#{user.username}", { :notice => "Thanks for signing up, " + user.name + "! Let's hit some goals!" })
+    else
+      redirect_to("/user_sign_up")
+    end
+  end
+
+  def show
+    the_username = params.fetch("the_username")
+    @user = User.where({ :name => the_username }).at(0)
+
+    render({ :template => "user_templates/show.html.erb" })
+  end
+
+  def index
+    @users = User.all.order({ :name => :asc })
+
+    render({ :template => "user_templates/index.html" })
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -9,9 +9,12 @@ Rails.application.routes.draw do
   get("/insert_goal", { :controller => "goals", :action => "create" })
   get("/modify_goal/:path_id", { :controller => "goals", :action => "update" })
 
+  get("/user_sign_up", { :controller => "users", :action => "new_registration_form" }) 
+
+
   get("/users", { :controller => "users", :action => "index" })
-  get("/users/:path_id", { :controller => "users", :action => "show" })
+  get("/users/:the_username", { :controller => "users", :action => "show" })
   get("/delete_user/:path_id", { :controller => "users", :action => "destroy" })
-  get("/insert_user", { :controller => "users", :action => "create" })
+  get("/insert_user_record", { :controller => "users", :action => "create" })
   get("/modify_user/:path_id", { :controller => "users", :action => "update" })
 end
